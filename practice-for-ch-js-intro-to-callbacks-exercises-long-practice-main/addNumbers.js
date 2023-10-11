0c7738b1e00574144e277e68e5103b0f9c68916f
yuris <yls4@cornell.edu>
2023-10-11 14:34:49 -0400

    completed addNumbers

---
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/addNumbers.js
@@ -0,0 +1,18 @@
+const readline = require(“readline”);
+const reader = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) {
+        reader.question('Give me a number', (answer) => {
+            let num = parseInt(answer);
+            sum += num;
+            console.log(sum);
+            addNumbers(sum, (numsLeft - 1), completionCallback);
+        }
+    }else {
+        completionCallback(sum);
+    }
+}
--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/clock.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/clock.js
@@ -1,33 +1,25 @@
 class Clock {
-    debugger
     constructor() {
-        debugger
         let date = new Date();
         this.hour = date.getHours();
         this.minutes = date.getMinutes();
         this.seconds = date.getSeconds();
         this.printTime();
-       
+        const tickbind = this._tick.bind(this);
         setInterval(this.tickbind, 1000);
-
     }
-    const tickbind = this._tick.bind(this);
     
-      
     printTime() {
         console.log(`${this.hour}:${this.minutes}:${this.seconds}` );
 
     }
     _tick() {
      this.seconds += 1;
-    //  console.log("Test");
-    // debugger;
      this.printTime();   
     }
-
-  
-
 }
+
+
 
 
 
